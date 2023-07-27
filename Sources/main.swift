e0a0d7d730c9a4eec3609ca3be97bbdd19d01ac8
Anto Christopher <anto@deepsource.io>
2023-07-27 17:03:07 +0530

    test

--- Sources/main.swift
+++ Sources/main.swift
@@ -15,4 +15,57 @@ func foo() {
     if otherVar != nil { }
     return 0
   }
+
+  if name.range(of: "John") != nil {
+    print("Name contains John")
+  }
+
+  if name.range(of: "Jane") == nil {
+      print("Name does not contain Jane")
+  }
+
+  if name.range(of: "John") != nil {
+      print("Name contains John")
+  }
+  if name.range(of: "Jane") == nil {
+      print("Name does not contain Jane")
+  }
+  if name.range(of: "John") != nil {
+    print("Name contains John")
+  }
+  
+  if name.range(of: "Jane") == nil {
+      print("Name does not contain Jane")
+  }
+
+  if name.range(of: "John") != nil {
+      print("Name contains John")
+  }
+  if name.range(of: "Jane") == nil {
+      print("Name does not contain Jane")
+  }
+
+  let numbers = [1, 2, 3, 4, 5]
+  let firstEven = numbers.filter { $0 % 2 == 0 }.first
+
+  let numbers = [1, 2, 3, 4, 5]
+  let firstEven = numbers.filter { $0 % 2 == 0 }.first
+
+  let numbers = [1, 2, 3, 4, 5]
+  let firstEven = numbers.filter { $0 % 2 == 0 }.first
+
+  let numbers = [1, 2, 3, 4, 5]
+  let firstEven = numbers.filter { $0 % 2 == 0 }.first
+
+  let arr = [1, 2, 3, 4, 5]
+  let lastEven1 = arr.filter { $0 % 2 == 0 }.last // returns 4
+
+  let arr = [1, 2, 3, 4, 5]
+  let lastEven1 = arr.filter { $0 % 2 == 0 }.last // returns 4
+
+  let arr = [1, 2, 3, 4, 5]
+  let lastEven1 = arr.filter { $0 % 2 == 0 }.last // returns 4
+
+  let arr = [1, 2, 3, 4, 5]
+  let lastEven1 = arr.filter { $0 % 2 == 0 }.last // returns 4
 }
